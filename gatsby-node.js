69bb2697514feadc22168085d321bd6f913ed628
Alex Kafer <me@alexkafer.com>
2020-05-18 11:12:52 -0500

    Remove unnecessary dev server

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-exports.modifyWebpackConfig = ({ config, stage }) => {
-    if (stage == "develop"){
-        config.devServer = {
-          hot: false, 
-          inline: false,
-      }
-    };
-    return config;
-};  
